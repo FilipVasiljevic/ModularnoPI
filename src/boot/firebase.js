fc6691be894b055b28692228ec3b3351d66b7aee
filip <vasiljevic.filip@gmail.com>
2023-01-19 18:38:46 +0100

    Prvi commit

---
+++ src/boot/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoXPNrI4hW8audtkmRSFRjw1GCLq_FPk8",
+  authDomain: "modularno-pi.firebaseapp.com",
+  projectId: "modularno-pi",
+  storageBucket: "modularno-pi.appspot.com",
+  messagingSenderId: "15882103093",
+  appId: "1:15882103093:web:3be556011f7d294a09c47c"
+};
+
+const app = initializeApp(firebaseConfig);
